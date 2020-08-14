27e8e99864105766cae23c18039d5726297fabc6
Fernando Crespo <fernando82@gmail.com>
2020-08-14 16:13:02 -0300

    Initial Commit

---
+++ CalMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CalMenu
+//
+//  Created by Fernando Crespo on 14/08/20.
+//  Copyright © 2020 Fernando Crespo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
